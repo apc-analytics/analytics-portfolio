e279bd37239fa3e781a1747cd536ae1e5190cbbe
Alex Cappola <279288290+apc-analytics@users.noreply.github.com>
2026-08-07 22:08:48 -0400

    Update engagement_and_value.sql for bug fixes

--- sql/apc_wellness/engagement_and_value.sql
+++ sql/apc_wellness/engagement_and_value.sql
@@ -28,18 +28,11 @@ with
             streak_recency_rank = 1
     )
 
-    , order_categories as (
+    , member_orders as (
         select
-            order_type
-            , case
-                when order_type = 'fitness_assessment' then 1
-                when order_type = 'coaching_session' then 1
-                when order_type = 'challenge_entry_fee' then 1
-                when order_type = 'challenge_entry_fee' then 0
-                else 0
-                end as is_one_time_order
-            , count(*)
-            , sum(amount) as total_amount
+            member_id
+            , count(*) as count_orders
+            , sum(amount) as total_orders_amount
         from
             db_portfolio.stg_apc_wellness.stg_apc_wellness__orders
         group by 1
@@ -50,16 +43,13 @@ select
     , p.tier
     , count(distinct m.member_id) as count_members
     , sum(s.billed_amount) as total_billed_amount
-    , round((sum(s.billed_amount) / 12), 2) as monthly_billed_amount
     , round((sum(s.billed_amount) / count(distinct m.member_id)), 2) as rev_per_member
 
     , count(case when mrs.is_current = 1 then m.member_id end) as count_mem_with_current_streak
     , max(case when mrs.is_current = 1 then mrs.streak_length_days end) as max_current_streak_length
-    , max(mls.longest_streak) as longest_streak_days
+    , round(avg(mls.longest_streak), 1) as avg_longest_streak_days
 
-    , count(case
-                when o.order_type IN('fitness_assessment', 'coaching_session', 'challenge_entry_fee')
-                    then o.order_id end) as count_one_time_orders
+    , sum(mo.count_orders) as count_orders -- these are one-time purchases instead of subscriptions
 from
     db_portfolio.stg_apc_wellness.stg_apc_wellness__members as m
         left join db_portfolio.stg_apc_wellness.stg_apc_wellness__accounts as a
@@ -68,8 +58,8 @@ from
             on m.member_id = s.member_id
         join db_portfolio.stg_apc_wellness.stg_apc_wellness__plans as p
             on s.plan_id = p.plan_id
-        left join db_portfolio.stg_apc_wellness.stg_apc_wellness__orders as o
-            on m.member_id = o.member_id
+        left join member_orders as mo
+            on m.member_id = mo.member_id
         left join most_recent_streaks as mrs
             on m.member_id = mrs.member_id
         left join member_longest_streaks as mls
